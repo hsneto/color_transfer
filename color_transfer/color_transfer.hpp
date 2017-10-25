02f0b173c2dedf6614d63fa92270baba886da1cc
Humberto Neto <humberto.nt4@gmail.com>
2017-10-25 10:40:29 -0200

    Update color_transfer.hpp

--- color_transfer/color_transfer.hpp
+++ color_transfer/color_transfer.hpp
@@ -6,65 +6,81 @@
 using namespace cv;
 using namespace std;
 
-vector<Scalar> imageStats(Mat image) {
-    Mat channel[3];
+vector<Scalar> imageStats(
+    const Mat& scr  // input image
+);
+
+void colorTransfer(
+    const Mat& scr, // source image (color that will be dst) 
+    const Mat& tgt, // targer image (image that will receive the new color)
+    Mat& dst        // output image
+);
+    
+vector<Scalar> imageStats(const Mat& image) {
+    Mat channels[3];
     Scalar mean[3], dev[3];
     vector<Scalar> meanDev;
 
-    split(image, channel);
+    split(image, channels);
     for (int i=0; i<3;i++) {
-        meanStdDev(channel[i], mean[i], dev[i]);
+        meanStdDev(channels[i], mean[i], dev[i]);
         meanDev.push_back(mean[i]);
         meanDev.push_back(dev[i]);        
     }
 
-    // meanDev = (mean(channel[0]), devStd(channel[0]), mean(channel[1]), devStd(channel[1]), mean(channel[1]), devStd(channel[1])) 
+        // meanDev = (
+            // mean(channel[0]),    -   mean from blue channel  
+            // devStd(channel[0]),  -   devStd from blue channel 
+            // mean(channel[1]),    -   mean from green channel 
+            // devStd(channel[1]),  -   devStd from green channel 
+            // mean(channel[1]),    -   mean from red channel 
+            // devStd(channel[1])   -   devStd from red channels 
+        // ) 
     return meanDev;
 }
 
-Mat colorTransfer(Mat source, Mat target) {
+void colorTransfer(const Mat& scr, const Mat& tgt, Mat& dst) {
+    Mat newScr, newTgt;
+
     // Convert the images from the RGB to L*ab*.
-    cvtColor(source, source, CV_BGR2Lab);
-    cvtColor(target, target, CV_BGR2Lab);
+    cvtColor(scr, newScr, CV_BGR2Lab);
+    cvtColor(tgt, newTgt, CV_BGR2Lab);
 
     // Convert the images to float32 
-    source.convertTo(source, CV_32F);   
-    target.convertTo(target, CV_32F);
+    newScr.convertTo(newScr, CV_32F);   
+    newTgt.convertTo(newTgt, CV_32F);
 
-    // Compute color statistics for the source and target images
-    vector<Scalar> meanDevScr = imageStats(source);
-    vector<Scalar> meanDevTar = imageStats(target);
+    // Compute color statistics for the scr and tgt images
+    vector<Scalar> meanDevScr = imageStats(newScr);
+    vector<Scalar> meanDevTgt = imageStats(newTgt);
 
-    // Split the target image into 3 channels (L, *a, *b) 
-    Mat channel[3];
-    split(target, channel);
+    // Split the tgt image into 3 channels (L, *a, *b) 
+    Mat channels[3];
+    split(newTgt, channels);
     
-    // Subtract the means from the target image
-	channel[0] -= meanDevTar[0];
-	channel[1] -= meanDevTar[2];
-    channel[2] -= meanDevTar[4];
+    // Subtract the means from the tgt image
+	channels[0] -= meanDevTgt[0];
+	channels[1] -= meanDevTgt[2];
+    channels[2] -= meanDevTgt[4];
  
     // Scale by the standard deviations
-    double a = meanDevTar[1](0) / meanDevScr[1](0);
-    double b = meanDevTar[3](0) / meanDevScr[3](0);
-    double c = meanDevTar[5](0) / meanDevScr[5](0);
+    double a = meanDevTgt[1](0) / meanDevScr[1](0);
+    double b = meanDevTgt[3](0) / meanDevScr[3](0);
+    double c = meanDevTgt[5](0) / meanDevScr[5](0);
 
-	channel[0] *= a;
-    channel[1] *= b;
-	channel[2] *= c;
+	channels[0] *= a;
+    channels[1] *= b;
+	channels[2] *= c;
      
-	// Add in the source mean
-	channel[0] += meanDevScr[0];
-	channel[1] += meanDevScr[2];
-    channel[2] += meanDevScr[4];
+	// Add in the scr mean
+	channels[0] += meanDevScr[0];
+	channels[1] += meanDevScr[2];
+    channels[2] += meanDevScr[4];
     
-	// Merge the channels together and convert back to the BGR color space.
-    Mat transfer;
-    merge(channel, 3, transfer);
-    transfer.convertTo(transfer, CV_8U);          
-    cvtColor(transfer, transfer, CV_Lab2BGR);
-
-    return transfer;
+	// Merge the channelss together and convert back to the BGR color space.
+    merge(channels, 3, dst);
+    dst.convertTo(dst, CV_8U);          
+    cvtColor(dst, dst, CV_Lab2BGR);
 }
 
 #endif //COLOR_TRANSFER_HPP
